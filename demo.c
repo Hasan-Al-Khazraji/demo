17a57d092ed82dfae045e7a62a7288ee27f9c9b4
Hasan Al-Khazraji <hasanwatheq2014@gmail.com>
2022-10-24 10:03:14 -0400

    now subtracts 73

--- demo.c
+++ demo.c
@@ -4,7 +4,7 @@ int main (){
     int num;
     printf("Please enter your favourite number: ");
     scanf(" %d", &num);
-    printf("Wrong your favourite number is %d.", num-2);
+    printf("Wrong your favourite number is %d.", num-73);
 
     return 0;
 }
